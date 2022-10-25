bee8e45706f4c22d086c244015fe93ef177b974b
faraj Shuaib <46637985+farajshuaib@users.noreply.github.com>
2022-10-25 21:34:49 +0200

    update footer

--- src/components/Header/LocalesDropDown.tsx
+++ src/components/Header/LocalesDropDown.tsx
@@ -3,8 +3,12 @@ import { locales } from "../../constant";
 import React, { Fragment } from "react";
 import { useTranslation } from "react-i18next";
 
-const LocalesDropDown: React.FC = () => {
-  const { i18n } = useTranslation();
+interface Props {
+  labeled?: boolean;
+}
+
+const LocalesDropDown: React.FC<Props> = ({ labeled }) => {
+  const { i18n, t } = useTranslation();
 
   const setLang = (lang: string) => {
     i18n.changeLanguage(lang);
@@ -21,10 +25,11 @@ const LocalesDropDown: React.FC = () => {
             <Popover.Button
               className={`
                 ${open ? "" : "text-opacity-90"}
-                 group  p-2 hover:bg-gray-100 dark:hover:bg-neutral-800 rounded-full inline-flex items-center text-base font-medium hover:text-opacity-100
+                 group  py-2 px-3 hover:bg-gray-100 gap-2 dark:hover:bg-neutral-800 rounded-full inline-flex items-center text-base font-medium hover:text-opacity-100
                   focus:outline-none relative`}
             >
               <i className="text-2xl bx bx-world"></i>
+              {labeled && <span className="">{t("Select_language")}</span>}
             </Popover.Button>
             <Transition
               as={Fragment}
@@ -35,20 +40,20 @@ const LocalesDropDown: React.FC = () => {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <Popover.Panel className="absolute z-10 w-screen max-w-xs px-4 mt-3 -right-28 sm:right-0 sm:px-0">
-                <div className="overflow-hidden shadow-lg rounded-2xl ring-1 ring-black ring-opacity-5">
-                  <div className="relative grid gap-4 p-5 bg-white dark:bg-neutral-800">
-                    <h3 className="text-xl font-semibold">languages</h3>
-                    {locales.map((item, index: number) => (
-                      <button
-                        key={index}
-                        onClick={() => setLang(item.key)}
-                        className={`${item.key == i18n.language && "bg-gray-100 dark:bg-gray-700"} relative flex px-4 py-2 transition duration-150 ease-in-out rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50`}
-                      >
-                        {item.value}
-                      </button>
-                    ))}
-                  </div>
+              <Popover.Panel className="absolute z-10 px-4 mt-3 -right-28 sm:right-0 sm:px-0">
+                <div className="relative grid gap-4 p-2 overflow-hidden bg-white shadow-lg rounded-2xl ring-1 ring-black ring-opacity-5 dark:bg-neutral-800">
+                  {locales.map((item, index: number) => (
+                    <button
+                      key={index}
+                      onClick={() => setLang(item.key)}
+                      className={`${
+                        item.key == i18n.language &&
+                        "bg-gray-100 dark:bg-gray-700"
+                      } relative flex  px-12 py-2 transition duration-150 ease-in-out rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50`}
+                    >
+                      {item.value}
+                    </button>
+                  ))}
                 </div>
               </Popover.Panel>
             </Transition>
--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,69 +1,51 @@
 import Logo from "../../shared/Logo/Logo";
 import SocialsList1 from "../../shared/SocialsList1/SocialsList1";
+import { CustomLink } from "../../data/types";
 import React from "react";
+import LocalesDropDown from "../../components/Header/LocalesDropDown";
+import { useTranslation } from "react-i18next";
 
 export interface WidgetFooterMenu {
   id: string;
   title: string;
-  menus: any[];
+  menus: CustomLink[];
 }
 
-const widgetMenus: WidgetFooterMenu[] = [
-  {
-    id: "5",
-    title: "Getting started",
-    menus: [
-      { href: "#", label: "Installation" },
-      { href: "#", label: "Release Notes" },
-      { href: "#", label: "Upgrade Guide" },
-      { href: "#", label: "Browser Support" },
-      { href: "#", label: "Editor Support" },
-      { href: "#", label: "Dark Mode" },
-    ],
-  },
-  {
-    id: "1",
-    title: "Explore",
-    menus: [
-      { href: "#", label: "Design features" },
-      { href: "#", label: "Prototyping" },
-      { href: "#", label: "Design systems" },
-      { href: "#", label: "Pricing" },
-      { href: "#", label: "Customers" },
-      { href: "#", label: "Security" },
-    ],
-  },
-  {
-    id: "2",
-    title: "Resources",
-    menus: [
-      { href: "#", label: "Best practices" },
-      { href: "#", label: "Support" },
-      { href: "#", label: "Developers" },
-      { href: "#", label: "Learn design" },
-      { href: "#", label: "What's new" },
-      { href: "#", label: "Releases" },
-    ],
-  },
-  {
-    id: "4",
-    title: "Community",
-    menus: [
-      { href: "#", label: "Discussion Forums" },
-      { href: "#", label: "Code of Conduct" },
-      { href: "#", label: "Community Resources" },
-      { href: "#", label: "Contributing" },
-      { href: "#", label: "Concurrent Mode" },
-      { href: "#", label: "API Reference" },
-    ],
-  },
-];
 
 const Footer: React.FC = () => {
+  const { t } = useTranslation();
+
+  const widgetMenus: WidgetFooterMenu[] = [
+    {
+      id: "5",
+      title: t("Getting_started"),
+      menus: [
+        { href: "/nfts", label: "NFTs" },
+        { href: "/collections", label: t("Collections") },
+        { href: "https://www.sahabanft.com", label: t("Website") },
+      ],
+    },
+    {
+      id: "1",
+      title: t("INFORMATION"),
+      menus: [
+        {
+          href: "https://www.sahabanft.com/term-of-services",
+          label: t("Term_of_services"),
+        },
+        {
+          href: "https://www.sahabanft.com/privacy-policy",
+          label: t("Privacy_policy"),
+        },
+        { href: "/kyc-form", label: t("Get_verified") },
+      ],
+    },
+  ];
+  
   const renderWidgetMenuItem = (menu: WidgetFooterMenu, index: number) => {
     return (
       <div key={index} className="text-sm">
-        <h2 className="font-semibold text-neutral-700 dark:text-neutral-200">
+        <h2 className="font-semibold uppercase text-neutral-700 dark:text-neutral-200">
           {menu.title}
         </h2>
         <ul className="mt-5 space-y-4">
@@ -73,6 +55,7 @@ const Footer: React.FC = () => {
                 key={index}
                 className="text-neutral-6000 dark:text-neutral-300 hover:text-black dark:hover:text-white"
                 href={item.href}
+                target="_blank"
               >
                 {item.label}
               </a>
@@ -84,17 +67,27 @@ const Footer: React.FC = () => {
   };
 
   return (
-    <div className="nc-Footer relative py-20 lg:pt-32 lg:pb-28 border-t border-neutral-200 dark:border-neutral-700">
-      <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 ">
-        <div className="grid grid-cols-4 gap-5 col-span-2 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
+    <div className="relative py-20 border-t nc-Footer lg:pt-32 lg:pb-28 border-neutral-200 dark:border-neutral-700">
+      <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-4 lg:gap-x-10 ">
+        <div className="grid grid-cols-4 col-span-2 gap-5 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
           <div className="col-span-2 md:col-span-1">
             <Logo />
           </div>
-          <div className="col-span-2 flex items-center md:col-span-3">
+          <div className="flex items-center col-span-2 md:col-span-3">
             <SocialsList1 className="flex items-center space-x-2 lg:space-x-0 lg:flex-col lg:space-y-3 lg:items-start" />
           </div>
         </div>
+
         {widgetMenus.map(renderWidgetMenuItem)}
+
+        <div className="text-sm">
+          <h2 className="font-semibold uppercase text-neutral-700 dark:text-neutral-200">
+            {t("Languages")}
+          </h2>
+          <div className="mt-5 space-y-4">
+            <LocalesDropDown labeled />
+          </div>
+        </div>
       </div>
     </div>
   );
